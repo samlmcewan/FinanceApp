f8eb07e94bb9771ec51c6b6e823d8229befbc8d8
samlmcewan <samlmcewan@gmail.com>
2021-10-05 12:06:03 +0100

    Chart renders top 10 merchants debit amounts

---
+++ wwwroot/scripts/chartRender.js
@@ -0,0 +1,113 @@
+console.log(topTen);
+
+// ['ACTIVE OUTDOOR PUR', 626]
+// 1: (2) ['BILLY BILSLAND CYC', 649.98]
+// 2: (2) ['CRO Topup', 700]
+// 3: (2) ['DELIVEROO', 752.27]
+// 4: (2) ['PLATINUM MASTERCAR', 800.3]
+// 5: (2) ['SAINSBURYS S/MKTS', 902.6]
+// 6: (2) ['HELP TO BUY ISA', 1000]
+// 7: (2) ['CRO', 2897.63]
+// 8: (2) ['MISS HEATHER DICKS', 2960]
+// 9: (2) ['S MCEWAN', 4700]
+
+
+// this code takes an array and turns into an object
+    //  const obj = Object.fromEntries(someArray);
+    //     console.log(obj); 
+    
+    
+    const topTenObj = Object.fromEntries(topTen);
+        console.log(topTenObj); 
+//         {
+            // ACTIVE OUTDOOR PUR: 626
+            // BILLY BILSLAND CYC: 649.98
+            // CRO: 2897.63
+            // CRO Topup: 700
+            // DELIVEROO: 752.27
+            // HELP TO BUY ISA: 1000
+            // MISS HEATHER DICKS: 2960
+            // PLATINUM MASTERCAR: 800.3
+            // S MCEWAN: 4700
+            // SAINSBURYS S/MKTS: 902.6
+//         }
+
+
+const labelsArray = [];
+topTen.forEach(item => labelsArray.push(item[0]));
+console.log(labelsArray);
+
+
+const debitAmountsArray = [];
+topTen.forEach(item => debitAmountsArray.push(item[1]));
+
+
+    //   topTen.forEach( (item) =>  Object.defineProperties( 
+    //       dataObj, { 
+    //           labels: 
+    //               {value: `${item[0]}`}
+    //       , 
+    //               property2: {
+                      
+    //               } 
+    //             }) );
+
+    //   console.log(dataObj);
+ 
+
+
+         const data = {
+            labels: labelsArray,
+            datasets: [{
+                label: 'Spending insights data',
+                data: debitAmountsArray,
+                backgroundColor: [
+                    
+                'rgb(0, 168, 255)',
+                
+                
+                'rgb(76, 209, 55)',
+                'rgb(127, 143, 166)',
+                'rgb(72, 126, 176)',
+                
+                'rgb(39, 60, 117)',
+                'rgb(53, 59, 72)',
+                'rgb(220, 221, 225)',
+                'rgb(156, 136, 255)',
+                'rgb(251, 197, 49)',
+                'rgb(232, 65, 24)',
+                
+                ],
+                hoverOffset: 4,
+                cutout: '33%'
+            }]
+            };
+            const config = {
+        type: 'doughnut',
+        data: data,
+        }; 
+           
+            var ctx = document.getElementById('myChart');
+
+            var myChart = new Chart(ctx, {
+                type: 'doughnut',
+                data: data,
+            });
+                        
+
+
+
+
+    // add up the the total debit amount values for all merchants in the chart, add it to the center div 
+    const totalSum = debitAmountsArray.reduce((partial_sum, a) => partial_sum + a,0); 
+    const totalNumDiv = document.querySelector('#totalNum');
+    console.log(totalNumDiv);
+    let totalNumP = document.createElement('p');
+    totalNumP.innerHTML = (totalSum);
+    totalNumDiv.appendChild(totalNumP);
+    
+      
+          
+
+
+            
---
+++ wwwroot/scripts/uploadHandler.js
@@ -0,0 +1,59 @@
+//   get the json from session storage and parse it  
+        let jsonObject = sessionStorage.getItem('transactions');
+        let parsedJson = JSON.parse(jsonObject);
+
+//  Iterate over the transactions and if its a debit transaction add it to the debitTransactions array  
+        let debitTransactions = [];
+        parsedJson.forEach((transaction) => {
+            if (transaction['Debit Amount'] > 0) {
+debitTransactions.push(transaction);
+            }
+        })
+       
+
+// Group the debit transactions by merchant
+        const groupByKey = (list, key) => list.reduce((hash, obj) => ({...hash, [obj[key]]:( hash[obj[key]] || [] ).concat(obj)}), {});
+        let merchantArrays = groupByKey(debitTransactions, 'Transaction Description');
+
+//  Get just the Merchant and its transactions
+
+    let merchantsTotalDebits = [];
+       for (const [merchant, transaction] of Object.entries(merchantArrays)) {
+           let transactions = [];
+             transaction.forEach((value) => {
+                transactions.push(value['Debit Amount']);
+            }); 
+   
+
+
+// Add up the total debit amounts for each merchant
+
+            const sum = transactions.reduce((partial_sum, a) => partial_sum + a,0); 
+// Make sum a string to 2 decimel places 
+            const sumFormat = sum.toFixed(2);
+            merchantsTotalDebits.push([merchant, parseFloat(sumFormat)]);
+           
+        };
+        console.log(merchantsTotalDebits);
+        
+// Take the merchantTOtalDebits array and sort from lowest to highest
+        merchantsTotalDebits.sort(function(a, b)
+	{
+	  return a[1]-b[1]; // compare numbers
+	});
+	 console.log(merchantsTotalDebits);
+
+
+
+// get the top 10 transactions with highest value into new array 
+     let topTen = merchantsTotalDebits.slice(-10);
+     console.log(topTen);
+
+     
+
+
+
+// this code takes an array and turns into an object
+    //  const obj = Object.fromEntries(someArray);
+    //     console.log(obj);  
+                        
